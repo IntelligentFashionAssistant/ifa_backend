a1d992e581bc43db24d966d2516b4f1d211a0145
KenanAL <56705071+KenanAL@users.noreply.github.com>
2021-12-02 20:45:56 +0200

    add Extension methods

---
+++ src/api/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using application.persistence;
+using repository.Repositories;
+
+namespace api
+{
+    public static class ExtensionMethods
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            //Repository
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IGarmentRepository, GarmentRepository>();
+            services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<IPropertyRepository, PropertyRepository>();
+
+        }
+    }
+}
